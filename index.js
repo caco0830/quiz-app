da3726820306a45f223252556d7201a3061f2966
Carlo Coria <carlo0830@gmail.com>
2019-06-22 09:48:36 -0700

    added style to current question and score display

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
-currentQuestion = 0;
-currentScore = 0;
+"use strict";
+let currentQuestion = 0;
+let currentScore = 0;
 
-function beginQuiz(){
-    $('.intro-screen').on('click', '.intro-button', function(event) {
+function startQuiz() {
+    beginQuiz();
+    //renderQuestion();
+    //console.log('quiz started');
+    getUserAnswer();
+    renderNextQuestion();
+    startNewQuiz();
+}
+
+function beginQuiz() {
+    $('.intro-screen').on('click', '.intro-button', function (event) {
         $('.intro-screen').remove();
-        $('.question-form').css('display', 'block');
+        $('.question-form').css('display', 'flex');
         $('.current-question').text(1);
+        renderQuestion();
     })
 }
 
 //render question
-function createQuestion(){
-    
- return `<div class="question-${currentQuestion}">
- <h2>${QUESTIONS[currentQuestion].question}</h2>
- <form>
+function createQuestion() {
+    return `<div class="question-${currentQuestion}">
+    <h2>${QUESTIONS[currentQuestion].question}</h2>
+    <form>
      <fieldset>
          <label for="">
              <input type="radio" value="${QUESTIONS[currentQuestion].answers[0]}" name="answer">
@@ -34,46 +44,45 @@ function createQuestion(){
          </label>
          <button type="submit">Submit Answer</button>
      </fieldset>
- </form>
-</div>`;
+    </form>
+    </div>`;
 }
 
-function renderQuestion(){
+function renderQuestion() {
     console.log(QUESTIONS.length);
     console.log(currentQuestion);
-    if(currentQuestion < QUESTIONS.length){
-        
+    if (currentQuestion < QUESTIONS.length) {
+
         $('.question-form').html(createQuestion);
         //updateQuestion();
-    //console.log('Question Rendered');
-    }else{
+        //console.log('Question Rendered');
+    } else {
         console.log('render Results');
-        
+
         renderResults();
-        
+
     }
 }
 
 //check answer display correct or wrong
-function getUserAnswer(){
-    $('form').on('submit', function(event) {
+function getUserAnswer() {
+    $('form').on('submit', function (event) {
         event.preventDefault();
-        let userAnswer = $('input:checked').val();
-        let correctAnswer = QUESTIONS[currentQuestion].correctAnswer;
+        const userAnswer = $('input:checked').val();
+        const correctAnswer = QUESTIONS[currentQuestion].correctAnswer;
 
-        if(userAnswer === correctAnswer){
+        if (userAnswer === correctAnswer) {
             console.log('Answer is correct');
             displayCorrect();
             updateScore();
-    
-        }else{
+        } else {
             console.log('Answer is incorrect');
             displayWrong();
         }
     });
 }
 
-function displayCorrect(){
+function displayCorrect() {
     $('.question-form').html(`<div class="feedback correct-answer">Correct Answer 
     <div class="block">Congrats!! Your Answer is correct</div>
     <div class="block">
@@ -82,9 +91,9 @@ function displayCorrect(){
     </div>`);
 }
 
-function displayWrong(){
+function displayWrong() {
     $('.question-form').html(`<div class="feedback wrong-answer">Wrong Answer 
-    <div class="block">Your Answer was not correct</div>
+    <div class="block">Your Answer was not correct. The correct answer is '${QUESTIONS[currentQuestion].correctAnswer}'</div>
     <div class="block">
         <button class="next">Next Question</button>
     </div>
@@ -92,52 +101,41 @@ function displayWrong(){
 }
 
 //update Score
-function updateScore(){
+function updateScore() {
     currentScore++;
     $('.score').text(currentScore);
 }
 
-function updateQuestion(){
-    console.log('Uupdating question number');
+function updateQuestion() {
+    console.log('Updating question number');
     currentQuestion++;
     $('.current-question').text(currentQuestion + 1);
 }
 
-function renderNextQuestion(){
-    $('main').on('click', '.next',function(event){
+function renderNextQuestion() {
+    $('main').on('click', '.next', function (event) {
         updateQuestion();
         renderQuestion();
         getUserAnswer();
-
-    }); 
+    });
 }
 
-function renderResults(){
+function renderResults() {
     $('.current-question').text(QUESTIONS.length);
     $('.question-form').html(`<div class="results">Results
-    <div class="block">Score: ${currentScore}/5</div>
-    <div class="block">
-        <button class="start-new">Begin new quiz</button>
-    </div>
-</div>`);
+        <div class="block">Score: ${currentScore}/5</div>
+            <div class="block">
+                <button class="start-new">Begin new quiz</button>
+            </div>
+        </div>`);
 
 }
 
-function startNewQuiz(){
-    $('.question-form').on('click', '.start-new', function(event){
+function startNewQuiz() {
+    $('.question-form').on('click', '.start-new', function (event) {
         console.log('start new');
         location.reload();
     });
-
-}
-
-function startQuiz(){
-    beginQuiz();
-    renderQuestion();
-    //console.log('quiz started');
-    getUserAnswer();
-    renderNextQuestion();
-    startNewQuiz();
 }
 
 $(startQuiz);
